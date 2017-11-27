c90efae694eac93ade97f8f271a243b3af3b9b8c
leez <hnslzy@gmail.com>
2017-11-28 03:26:51 +0900

    3.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,8 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'ko-kr'
 
 #TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Seoul'
